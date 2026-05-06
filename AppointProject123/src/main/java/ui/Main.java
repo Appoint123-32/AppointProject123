383b6e581b173767fdaea287415cb9c67731a781
TuqaGh0 <tugha7@gmail.com>
2026-05-06 15:11:32 +0300

    Rename main class to Main and fix casing

--- AppointProject123/src/main/java/ui/Main.java
+++ AppointProject123/src/main/java/ui/Main.java
@@ -11,7 +11,7 @@ import repository.AppointmentRepository;
 import service.AuthenticationService;
 import service.BookingService;
 
-public class main {
+public class Main {
 
     public static void main(String[] args) {
 
